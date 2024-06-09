baf3d3e4137892055079cfc44792abc453e81ede
s24009 <s24009@std.it-college.ac.jp>
2024-06-10 06:48:21 +0900

    add if_kadai.py

---
+++ if_kadai.py
@@ -0,0 +1,12 @@
+#s24009
+#スコア判定コマンド
+score = 70
+if score >= 80:
+    print("優")
+elif score >= 70:
+    print("良")
+elif score >= 60:
+    print("可")
+else:
+    print("不可")
+    
---
+++ score.py
@@ -0,0 +1,9 @@
+#s24009
+#スコア判定コマンド
+if score >= 80:
+    print("優")
+elif score >= 60:
+    print("可")
+else:
+    print("不可")
+    
