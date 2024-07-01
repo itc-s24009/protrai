30ea4c88c70c32f9d451ebc3ba45c889f839c4ad
s24009 <s24009@std.it-college.ac.jp>
2024-07-01 16:10:36 +0900

    add dispImageKadai01.py

---
+++ dispImageKadai01.py
@@ -0,0 +1,26 @@
+#24009
+# dispImage.py
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image, PIL.ImageTk
+def dispPhoto(path):
+    newImage = PIL.Image.open(path).resize((300,300))
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image = imageData)
+    imageLabel.image = imageData
+def openFile():
+    fpath = fd.askopenfilename()
+    if fpath:
+        dispPhoto(fpath)
+        print(fpath)
+        lbl3 = tk.Label(text=fpath)
+        lbl3.pack()
+root = tk.Tk()
+root.geometry("400x400")
+lbl2 = tk.Label(text="🎨画像表示アプリ バージョン2.0🎨")
+btn = tk.Button(text="ファイルを開く", command = openFile)
+imageLabel = tk.Label()
+lbl2.pack()
+btn.pack()
+imageLabel.pack()
+tk.mainloop()
